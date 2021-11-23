96af1fe15ad31ba8aaad18b94eb259d70fb28f62
YooE <yooe05@gmail.com>
2021-11-23 10:43:51 +0300

    first commmit

---
+++ Assets/Snapp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] 
+
+public class Snapp : MonoBehaviour
+{
+    [Range(1f, 20f)]
+    public float rangePos = 1f;
+
+    void Update()
+    {
+
+
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / rangePos) * rangePos;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / rangePos) * rangePos;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
